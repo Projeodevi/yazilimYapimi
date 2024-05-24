20b9a3930f25925b00925cde05c73b1ce8f0b46e
sena <114770593+Annesrun@users.noreply.github.com>
2024-05-24 23:28:01 +0300

    Update Header.js

--- word-memorization-game-backend/Header.js
+++ word-memorization-game-backend/Header.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React from 'react'; //kullancı arayüzü oluşturma//
 function Header() {
   return <header>Header</header>;
 }
+git add src/components/Header.js
+git commit -m "Add Header component"
+git push origin main
 
 export default Header;
